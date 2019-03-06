e6fdb13079fba572c982d87e9bbaa2435715d731
aameirh <amirkaashu@gmail.com>
2019-03-06 15:04:06 +0530

    Uploading My Portfolio Website

---
+++ jQuerySimpleCounter.js
@@ -0,0 +1,18 @@
+$('#count-example').jQuerySimpleCounter({
+
+    // start number
+    start:  0,
+  
+    // end number
+    end:    100,
+  
+    // easing effect
+    easing: 'swing',
+  
+    // duration time in ms
+    duration: 400,
+  
+    // callback function
+    complete: ''
+  
+  });
